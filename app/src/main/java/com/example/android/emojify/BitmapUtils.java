148e3fe6306b68fac54bd25661b5391ce4a46633
Osaro <saro.iyoha@gmail.com>
2018-08-16 19:41:54 +0100

    add helpful comments

--- app/src/main/java/com/example/android/emojify/BitmapUtils.java
+++ app/src/main/java/com/example/android/emojify/BitmapUtils.java
@@ -43,6 +43,9 @@ class BitmapUtils {
     /**
      * Resamples the captured photo to fit the screen for better memory usage.
      *
+     * more: The resamplePic() method gets the height and width of the device screen in pixels,
+     * and resamples the passed in image to fit the screen.
+     *
      * @param context   The application context.
      * @param imagePath The path of the photo to be resampled.
      * @return The resampled bitmap
@@ -56,34 +59,46 @@ class BitmapUtils {
         WindowManager manager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
         manager.getDefaultDisplay().getMetrics(metrics);
 
+        //From DisplayMetrics Android Doc:
+        // The absolute height & width of the available display size in pixels
         int targetH = metrics.heightPixels;
         int targetW = metrics.widthPixels;
 
-        /**From the android docs*
-         *Creates Bitmap objects from various sources, including files, streams, and byte-arrays.
-         */
+        //From the android docs*
+        //BitmapFactory Creates Bitmap objects from various sources, including files, streams, and byte-arrays.
 
         // Get the dimensions of the original bitmap
         BitmapFactory.Options bmOptions = new BitmapFactory.Options();
         bmOptions.inJustDecodeBounds = true;
+
         //Decode a file path into a bitmap.
         // If the specified file name is null, or cannot be decoded into a bitmap, the function returns null.
         BitmapFactory.decodeFile(imagePath, bmOptions);
+        //The resulting width of the bitmap. If inJustDecodeBounds is set to false, this will be width of the output bitmap after any scaling is applied.
+        // If true, it will be the width of the input image without any accounting for scaling. Same idea for outHeight
         int photoW = bmOptions.outWidth;
         int photoH = bmOptions.outHeight;
 
         // Determine how much to scale down the image
         int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
 
+        //TODO: log.e the height and width ofthe image and phone screen into the console
+        //To see how the scale factor is gotten
+
         // Decode the image file into a Bitmap sized to fill the View
         bmOptions.inJustDecodeBounds = false;
+        // bmOptions.inSampleSize
+        // If set to a value > 1, requests the decoder to subsample the original image, returning a smaller image to save memory.
+        // The sample size is the number of pixels in either dimension that correspond to a single pixel in the decoded bitmap.
+        // For example, inSampleSize == 4 returns an image that is 1/4 the width/height of the original, and 1/16 the number of pixels.
+        // Any value <= 1 is treated the same as 1. Note: the decoder uses a final value based on powers of 2, any other value will be rounded down to the nearest power of 2.
         bmOptions.inSampleSize = scaleFactor;
 
         return BitmapFactory.decodeFile(imagePath);
     }
 
     /**
-     * Creates the temporary image file in the cache directory.
+     * Creates the temporary image file in the cache directory and returns the new temp file.
      *
      * @return The temporary image file.
      * @throws IOException Thrown if there is an error creating the file
@@ -94,6 +109,8 @@ class BitmapUtils {
         String imageFileName = "JPEG_" + timeStamp + "_";
         File storageDir = context.getExternalCacheDir();
 
+        //Creates a new empty file in the specified directory,
+        // using the given prefix and suffix strings to generate its name.
         return File.createTempFile(
                 imageFileName,  /* prefix */
                 ".jpg",         /* suffix */
@@ -130,9 +147,14 @@ class BitmapUtils {
      * @param imagePath The path of the saved image
      */
     private static void galleryAddPic(Context context, String imagePath) {
+        //ACTION_MEDIA_SCANNER_SCAN_FILE constant value to scan a file
+        //and add it to the media database. It is a Broadcast Action
         Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
         File f = new File(imagePath);
+
+        //Creates a Uri from a file
         Uri contentUri = Uri.fromFile(f);
+
         mediaScanIntent.setData(contentUri);
         context.sendBroadcast(mediaScanIntent);
     }
@@ -166,7 +188,11 @@ class BitmapUtils {
             File imageFile = new File(storageDir, imageFileName);
             savedImagePath = imageFile.getAbsolutePath();
             try {
+                //OutputStream is an abstract class that represents writing output
                 OutputStream fOut = new FileOutputStream(imageFile);
+                //  Bitmap.compress
+                // Write a compressed version of the bitmap to the specified outputstream.
+                // If this returns true, the bitmap can be reconstructed by passing a corresponding inputstream to BitmapFactory.decodeStream().
                 image.compress(Bitmap.CompressFormat.JPEG, 100, fOut);
                 fOut.close();
             } catch (Exception e) {
